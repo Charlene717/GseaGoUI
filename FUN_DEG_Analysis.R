e5aaedebf62646e743bf9721e10d0eb66b8eba42
Charlene <q2330416@gmail.com>
2022-11-03 21:46:39 +0800

    pair Setting Modify the defult setting

--- FUN_DEG_Analysis.R
+++ FUN_DEG_Analysis.R
@@ -4,26 +4,37 @@
 
 FUN_DEG_Analysis = function(GeneExp.df, Anno.df,
                             GroupType = AnnoSet.lt[["GroupType"]], GroupCompare = AnnoSet.lt[["GroupCompare"]],
-                            ThrSet = DEGThr.lt,
-                            TarGeneName = TarGene_name, GroupMode = Mode_Group, SampleID = "sampleID",
-                            Save.Path = Save.Path, ExportName = ExportName, AnnoName = "AvB"
+                            ThrSet = Thr.lt,
+                            TarGeneName = TarGene_name, GroupMode = Mode_Group, SampleID = "X_INTEGRATION",
+                            Save.Path = Save.Path, SampleName = SampleName, AnnoName = "AvB"
 ){
 
-  ##### Load Packages  #####
-  source("FUN_Package_InstLoad.R")
-  Basic.set <- c("tidyverse","ggplot2")
-  BiocManager.set <- c("edgeR","baySeq")
-  FUN_Package_InstLoad(Basic.set = Basic.set, BiocManager.set = BiocManager.set)
+  ##### Parameter setting* #####
+  # Set the desired organism
+  organism = "org.Dm.eg.db"
 
-  #************************************************************************************************************************#
 
+  #### BiocManager installation ####
+  ## Check whether the installation of those packages is required from BiocManager
+  if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  # BiocManager::install()
+  Package.set <- c(organism,"edgeR","baySeq")
+  for (i in 1:length(Package.set)) {
+    if (!requireNamespace(Package.set[i], quietly = TRUE)){
+      BiocManager::install(Package.set[i])
+    }
+  }
+  ## Load Packages
+  lapply(Package.set, library, character.only = TRUE)
+  rm(Package.set,i)
 
   #### Differential Expression Gene Analysis ####
   library(edgeR)
   Anno_Ints.df <- Anno.df[Anno.df[,GroupType] %in% GroupCompare,]
   # Anno_Ints.df <- Anno.df[Anno.df$ReCluster %in% c("AD","AC"),]  # c("CoreCD00","CDOri")
   matrix_Ints.df <- GeneExp.df
-  # colnames(matrix_Ints.df) <-  gsub("\\.", "-", colnames(matrix_Ints.df))
+  colnames(matrix_Ints.df) <-  gsub("\\.", "-", colnames(matrix_Ints.df))
   matrix_Ints.df <- matrix_Ints.df[,colnames(matrix_Ints.df) %in% Anno_Ints.df[,SampleID]]
 
   matrix_Ints_ID.df <- data.frame(ID = colnames(matrix_Ints.df))
@@ -32,7 +43,7 @@ FUN_DEG_Analysis = function(GeneExp.df, Anno.df,
   Anno_Ints.df <- left_join(matrix_Ints_ID.df, Anno_Ints.df)
 
   DGE_Ints.lt <- DGEList(counts=matrix_Ints.df, group=Anno_Ints.df[,GroupType], lib.size=rep(1000,ncol(matrix_Ints.df)))
-  DE_Extract.lt <- exactTest(DGE_Ints.lt,pair = GroupCompare, dispersion=0.2)
+  DE_Extract.lt <- exactTest(DGE_Ints.lt,pair = GroupCompare %>% as.character(), dispersion=0.2)
   DE_Extract.df <- topTags(DE_Extract.lt,n = nrow(matrix_Ints.df)) %>%
                            as.data.frame() %>%
                            data.frame(Gene=row.names(.),.)
@@ -54,11 +65,11 @@ FUN_DEG_Analysis = function(GeneExp.df, Anno.df,
   DE_Extract_FltL.set <- rownames(DE_Extract_FltL.df)
 
 
-  #### Export file ####
-  write.table(DE_Extract.df, file = paste0(Save.Path,"/DEGAnalysis_",ExportName,"_",AnnoName,".tsv"),
-              sep="\t", row.names= F, quote = FALSE)
-  write.table(DE_Extract_Flt.df, file = paste0(Save.Path,"/DEGAnalysis_Flt_",ExportName,"_",AnnoName,".tsv"),
-              sep="\t", row.names= F, quote = FALSE)
+  # #### Export file ####
+  # write.table(DE_Extract.df, file = paste0(Save.Path,"/DEGAnalysis_",SampleName,"_",AnnoName,".tsv"),
+  #             sep="\t", row.names= F, quote = FALSE)
+  # write.table(DE_Extract_Flt.df, file = paste0(Save.Path,"/DEGAnalysis_Flt_",SampleName,"_",AnnoName,".tsv"),
+  #             sep="\t", row.names= F, quote = FALSE)
 
 
   #### Output ####
@@ -67,7 +78,7 @@ FUN_DEG_Analysis = function(GeneExp.df, Anno.df,
   Output[["DE_Extract_Flt.df"]] <- DE_Extract_Flt.df
   Output[["DE_Extract_FltH.set"]] <- DE_Extract_FltH.set
   Output[["DE_Extract_FltL.set"]] <- DE_Extract_FltL.set
-  Output[["Thr.lt"]] <- ThrSet
+  # Output[["Thr.lt"]] <- ThrSet
 
   return(Output)
 
