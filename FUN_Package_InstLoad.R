87711e329819fbb03a378588efc5b8bad80b3d46
Charlene <q2330416@gmail.com>
2023-01-18 17:13:44 +0800

    FUN

---
+++ FUN_Package_InstLoad.R
@@ -0,0 +1,37 @@
+## Load Packages
+#
+FUN_Package_InstLoad = function( Basic.set = Basic.set,
+                             BiocManager.set = BiocManager.set)
+{
+
+  ##### For few packages #####
+  # if(!require("tidyverse")) install.packages("tidyverse")
+  # library(tidyverse)
+
+  #### Basic installation ####
+  ## Check whether the installation of those packages is required from basic
+  for (i in 1:length(Basic.set)) {
+    if (!requireNamespace(Basic.set[i], quietly = TRUE)){
+      install.packages(Basic.set[i])
+    }
+  }
+  ## Load Packages
+  lapply(Basic.set, library, character.only = TRUE)
+  rm(Basic.set,i)
+
+
+  #### BiocManager installation ####
+  ## Check whether the installation of those packages is required from BiocManager
+  if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  BiocManager.set <- BiocManager.set
+  for (i in 1:length(BiocManager.set)) {
+    if (!requireNamespace(BiocManager.set[i], quietly = TRUE)){
+      BiocManager::install(BiocManager.set[i])
+    }
+  }
+  ## Load Packages
+  lapply(BiocManager.set, library, character.only = TRUE)
+  rm(BiocManager.set,i)
+
+}
